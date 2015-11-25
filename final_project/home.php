f3bbf26f46d53ede9a6c57bd890ebfd3b537aa19
unknown <ottoliarobert@gmail.com>
2015-11-24 19:29:09 -0800

    Working home page

---
+++ final_project/home.php
@@ -0,0 +1,45 @@
+<?php
+    include 'template.php';
+?>
+
+
+<html>
+    <head>
+        <link rel="stylesheet" href="./public/css/home.css">
+    </head>
+    <body>
+        <div id="main">
+            <h2>Welcome to the Micro-Brewery Database!</h2>
+            <h3>
+                <p>
+                    We are committed to giving you the best & most accurate
+                    information on micro-breweries.  Whether you want to find
+                    local beers or find what beers are popular with the employees,
+                    we have got you covered! We also provide a list of salaries for
+                    of the brewery employees, along with amenities that the brewery
+                    provides.  Please help us expand our database for future use by
+                    clicking the 'add' tab above and then entering your information!
+                </p>
+            </h3>
+            <h4>
+                Top 3 Rated Breweries:
+            </h4>
+            <ol>
+                <?php
+                $query = <<<stmt
+                SELECT name, rating FROM breweries
+                ORDER BY rating DESC limit 3
+stmt;
+                $stmt = $mysql->prepare($query);
+                $stmt->execute();
+                $stmt->bind_result($name, $rating);
+                while ($stmt->fetch()) {
+                    echo <<<res
+                    <li>Name: $name Rating: $rating/5</li>
+res;
+                }
+                ?>
+            </ol>
+        </div>
+    </body>
+</html>

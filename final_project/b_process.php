ea8c21d1ba72f4a3522241a46109a1466d99f792
unknown <ottoliarobert@gmail.com>
2015-11-25 21:52:12 -0800

    Beer filter processing done

---
+++ final_project/b_process.php
@@ -0,0 +1,109 @@
+<?php
+    include 'template.php';
+?>
+
+<table class="table table-striped">
+    <tr>
+        <th>Beer Name</th>
+        <th>Type</th>
+        <th>Style</th>
+        <th>ABV%</th>
+        <th>Found At</th>
+    </tr>
+<?php
+    if ($_GET["b_type"] != "Select Beer Type" && $_GET["b_style"] != "Select Beer Style"
+        && $_GET["b_range"] != "Select ABV% Range") {
+
+        $type = mysqli_real_escape_string($mysql, $_GET["b_type"]);
+        $style = mysqli_real_escape_string($mysql, $_GET["b_style"]);
+        $abv = mysqli_real_escape_string($mysql, $_GET["b_range"]);
+        $lo = 0;
+        $hi = 0;
+
+        if ($abv == "0-5") {
+            $lo = 0; $hi = 5;
+        } else if ($abv == "5-10") {
+            $lo = 5; $hi = 10;
+        } else {
+            $lo = 10; $hi = 100;
+        }
+
+        $query = <<<stmt
+        SELECT beers.name, beers.type, beers.style, beers.abv, GROUP_CONCAT(breweries.name) AS found_at
+        FROM breweries INNER JOIN serves on breweries.id = serves.brewery_id INNER JOIN beers ON
+        beers.id = serves.beer_id WHERE beers.type = "$type" AND beers.style = "$style" AND beers.abv > $lo
+        AND beers.abv < $hi GROUP BY beers.name;
+stmt;
+        $stmt = $mysql->prepare($query);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($name, $style, $type, $abv, $found_at);
+        $num_rows = $stmt->num_rows;
+        if ($num_rows > 0) {
+            while ($stmt->fetch()) {
+
+                echo <<<res
+                <tr>
+                    <td>$name</td>
+                    <td>$style</td>
+                    <td>$type</td>
+                    <td>$abv</td>
+                    <td>$found_at</td>
+                </tr>
+res;
+            }
+            exit();
+
+        } else {
+
+            echo <<<res
+            <h2>No matching beers were found in the database with your filters.
+            Please go back and re-filter, or add a beer that you know matches
+            the criteria!</h2>
+res;
+            exit();
+
+        }
+
+    } else {
+
+        $query = <<<stmt
+        SELECT beers.name, beers.type, beers.style, beers.abv, GROUP_CONCAT(breweries.name) AS found_at
+        FROM breweries INNER JOIN serves on breweries.id = serves.brewery_id INNER JOIN beers ON
+        beers.id = serves.beer_id GROUP BY beers.name;
+stmt;
+        $stmt = $mysql->prepare($query);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($name, $style, $type, $abv, $found_at);
+        $num_rows = $stmt->num_rows;
+        if ($num_rows > 0) {
+
+            while ( $stmt->fetch() ) {
+
+                echo <<<res
+            <tr>
+                <td>$name</td>
+                <td>$style</td>
+                <td>$type</td>
+                <td>$abv</td>
+                <td>$found_at</td>
+            </tr>
+res;
+            }
+            exit();
+
+        } else {
+
+            echo <<<res
+            <h2>No matching beers were found in the database with your filters.
+            Please go back and re-filter, or add a beer that you know matches
+            the criteria!</h2>
+res;
+            exit();
+
+        }
+    }
+?>
+
+</table>
